3882a5f768e33da892ed4b4fd4d89a764da656da
Angi <nailmaker.gigi@gmail.com>
2018-02-01 20:17:13 +0100

    landing refactor, index scrolling under construction...

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
+app.get("/index", function(req, res){
+    res.render("index");
 });
 
 app.listen(process.env.PORT || 4000, process.env.IP || "", function(){
